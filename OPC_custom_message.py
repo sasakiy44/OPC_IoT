cdeb39fbae25ff3b7ff1c73f90a53fd74bd4c619
sasakiy44 <sasakiy@st.kobedenshi.ac.jp>
2025-08-25 14:14:10 +0900

    Update OPC_custom_message.py

--- OPC_custom_message.py
+++ OPC_custom_message.py
@@ -1,2 +1,4 @@
 # メッセージのカスタマイズ 61行目をこれで上書きコピーしよう
-            print(f"距離: {distance:.2f} cm - {'近すぎるよ！' if distance < 10 else 'ちょうどいい' if distance <= 50 else '遠いね'}")
+            print(f"距離: {distance:.2f} cm - \
+            {'近すぎるよ！' if distance < 10 \
+            else 'ちょうどいい' if distance <= 50 else '遠いね'}")
